13e96836d2495eeeec74445df2f004f76acd0ee1
Mateusz Paulus <paulus.m.mateusz@gmail.com>
2024-06-28 19:13:56 +0200

    Feature: Delete unused connection

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({path: './config.env'});
 const app = require('./app');
-const {connection} = require('mongoose');
 
 const DB = process.env.DB.replace('<PASSWORD>', process.env.DB_PASSWORD);
 
 mongoose
   .connect(DB)
-  .then(connection => {
+  .then(() => {
     console.log('MongoDB connection connected to MongoDB');
   });
 const port = process.env.PORT || 3000;
